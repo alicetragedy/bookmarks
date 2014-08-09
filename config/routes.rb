2c47b32bc4534bde5906acaeb99739edc95e43e1
alicetragedy <girl@alicetragedy.org>
2014-08-09 20:11:47 +0200

    devise configuration, add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bookmarks::Application.routes.draw do
+
+  devise_for :users
+  root :to => 'bookmarks#index'
+
   resources :bookmarks
 
   # The priority is based upon order of creation: first created -> highest priority.
